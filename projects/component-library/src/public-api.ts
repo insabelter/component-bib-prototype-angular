ce607f346ef9df5d633760b9cf38bd6aa0a4e42f
Insa Belter <insa.belter@atos.net>
2022-07-12 11:07:19 +0200

    first test version

---
+++ projects/component-library/src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of component-library
+ */
+
+export * from './lib/components/test/test.module';
+export * from './lib/components/test/test.component';
+
+export * from './lib/components/test2/test2.module';
+export * from './lib/components/test2/test2.component';
